2b050d387babf9eca835ac2ff041d7865e74d7e7
Jeffrey Souza <jeffrey@alunos.utfpr.edu.br>
2025-09-05 09:28:32 -0300

    - move RestTemplate bean from Application to dedicated config class

--- src/main/java/com/serasa/desafio/DesafioApplication.java
+++ src/main/java/com/serasa/desafio/DesafioApplication.java
@@ -6,13 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class DesafioApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SpringApplication.run(DesafioApplication.class, args);
     }
 }
